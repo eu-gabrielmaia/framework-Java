bb2f0472661aa1f61bd15d99e14f4f3c1af78d25
Gabriel Maia <140010302+gabrielmaia7g@users.noreply.github.com>
2024-09-09 15:05:19 -0300

    corrigindo erro ao executar código

--- TesteHibernate/TesteHibernate/src/testehibernate/TesteHibernate2.java
+++ TesteHibernate/TesteHibernate/src/testehibernate/TesteHibernate2.java
@@ -31,14 +31,14 @@ public class TesteHibernate2 {
             Aluno al3 =new Aluno(9012,"Maria");
 
             //cria professores
-            /*
+
             Professor p1 = new Professor(1, "Matioli",3000,8);
             Professor p2 = new Professor(2, "Priscila",27000,6);
             Professor p3 = new Professor(3, "Simone",30000,8);
 
             //cria cliente
-            Cliente c1 = new Cliente(1, "Gabriel", 17, 11);
-            */
+            //Cliente c1 = new Cliente(1, "Gabriel", 17, 11);
+
 
 
 
@@ -52,13 +52,12 @@ public class TesteHibernate2 {
         session.save(disc2);
         session.save(disc3);
 
-        /*
         session.save(p1);
         session.save(p2);
         session.save(p3);
 
-        session.save(p3);
-        */
+        //session.save(p3);
+
            
         session.getTransaction().commit();
         
